04c195a164a7a03fda645426add5c7ac84365395
ins-mniac <107356715+ins-mniac@users.noreply.github.com>
2022-06-15 23:21:06 -0500

    Hu Tao's E causes two instances of "Avatar_Hutao_CrimsonPlum" to be invoked when using data.getParentAbilityName().getStr()
    It is likely that there is a difference in the two AbilityMetaModifierChange protobuf objects, so I need to distinguish from the ability activation event and the unneeded event.
    
    Signed-off-by: ins-mniac <107356715+ins-mniac@users.noreply.github.com>

--- src/main/java/emu/grasscutter/game/ability/SelfDamageAbilityManager.java
+++ src/main/java/emu/grasscutter/game/ability/SelfDamageAbilityManager.java
@@ -5,29 +5,38 @@ import emu.grasscutter.game.player.Player;
 import emu.grasscutter.game.props.FightProperty;
 import emu.grasscutter.net.proto.AbilityInvokeEntryOuterClass.AbilityInvokeEntry;
 import emu.grasscutter.net.proto.AbilityMetaModifierChangeOuterClass.AbilityMetaModifierChange;
+import lombok.Data;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class SelfDamageAbilityManager {
 
     private final Player player;
-    private final HashMap<String, Float> damageAvatarList;
-//    private final ArrayList<String> cooldownList;
+    private final ArrayList<avatar> avatars;
 
-//    private class avatar{
-//        String name;
-//        float damagePercent;
-//
-//    }
+    @Data
+    private class avatar {
+        String name;
+        float damagePercent;
+        int id;
+
+        public avatar(String name, float damagePercent, int id) {
+            this.name = name;
+            this.damagePercent = damagePercent;
+            this.id = id;
+        }
+    }
 
     public SelfDamageAbilityManager(Player player) {
         this.player = player;
-//        cooldownList = new ArrayList<>();
-        damageAvatarList = new HashMap<>();
-        //Avatar name, damage amount
-        damageAvatarList.put("Hutao", .30f /* 30% of Hu Tao's health */);
+
+        avatars = new ArrayList<>();
+
+        avatars.add(new avatar("Hutao", .30f, 9));
+        avatars.add(new avatar("Shinobu", .30f, 5));
+        //To add: Xiao?
 
     }
 
@@ -36,25 +45,29 @@ public class SelfDamageAbilityManager {
         AbilityMetaModifierChange data = AbilityMetaModifierChange.parseFrom(invoke.getAbilityData());
 
         if (data == null) return;
-        if (data.getParentAbilityName().getStr().isBlank())
-            return; // Also functions as an == null check, though getParentAbilityName is NotNull? /shrug
+        if (data.getParentAbilityName().getStr().isBlank()) return;
 
         System.out.println(data);
         String modifierString = data.getParentAbilityName().getStr();
-        System.out.println(modifierString);
-        for (String avatarName : damageAvatarList.keySet()) {
-            if (modifierString.contains(avatarName)) {
+//        System.out.println(modifierString);
+
+        for (avatar avatar : avatars) {
+            if ((modifierString.contains(avatar.getName())) && (data.getModifierLocalId() == avatar.getId())) {
 
                 List<EntityAvatar> activeTeam = player.getTeamManager().getActiveTeam();
                 int currentIndex = player.getTeamManager().getCurrentCharacterIndex();
                 EntityAvatar currentAvatar = activeTeam.get(currentIndex);
 
                 float currentHealth = currentAvatar.getFightProperty(FightProperty.FIGHT_PROP_CUR_HP);
-                float damageAmount = damageAvatarList.get(avatarName) * currentHealth;
+                float damageAmount = avatar.getDamagePercent() * currentHealth;
 
                 if (!(currentHealth - damageAmount <= 1)) {
-                    currentAvatar.damage(damageAvatarList.get(avatarName) * currentHealth);
-//                    cooldownList.add(modifierString);
+                    //reduce health
+                    currentAvatar.damage(damageAmount);
+                    System.out.printf("damaging %s%n",avatar.getName());
+                } else {
+                    //set health to 1
+                    currentAvatar.damage(currentHealth - (currentHealth - 1));
                 }
             }
         }
